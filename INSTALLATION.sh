27c280637babd0b6a0206b53be3bace832973477
std-yong <std-yong@github.com>
2023-03-21 05:49:23 +0000

    gamst

---
+++ INSTALLATION.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo start mkdir.sh
+sh mkdir.sh
+sleep 2s
+echo start movefiles.sh
+sh movefiles.sh
+sleep 2s
+echo start StartApp.sh
+sh StartApp.sh
+sleep 2s
+echo start Makemigration.sh
+sh Makemigration.sh
+sleep 2s
+echo start Makemodelview.sh
+sh Makemodelview.sh
+sleep 2s 
+echo start Makefrontend.sh
+sh Makefrontend.sh
+sleep 2s
+echo docker-compose UP!
+docker-compose up --build
