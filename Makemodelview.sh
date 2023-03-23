a11484620f5f0591ff15679d44214e28de5dd62b
seungsura <seungsura@github.com>
2023-03-23 01:07:30 +0000

    reset

--- Makemodelview.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cat source/backend.web.config.urls.py > backend/web/config/urls.py
-cat source/backend.web.todo.urls.py > backend/web/todo/urls.py
-cat source/backend.web.todo.serializers.py > backend/web/todo/serializers.py
-cat source/backend.web.todo.views.py > backend/web/todo/views.py
-docker-compose run --rm web sh -c "echo yes | python manage.py collectstatic"
-docker-compose up -d --build
--- StartApp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker-compose run --rm web sh -c "django-admin startproject config ."
-docker-compose run --rm web sh -c "python manage.py startapp todo"
-touch backend/web/config/local_settings.py
-cat source/local_settings.py > backend/web/config/local_settings.py
-
-docker-compose up -d --build
